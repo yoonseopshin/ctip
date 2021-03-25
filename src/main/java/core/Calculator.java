f2a92e4b04180fdd42921fe53a96f0561b2b9ac9
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-03-25 18:28:41 +0900

    Linter: google style

--- src/main/java/core/Calculator.java
+++ src/main/java/core/Calculator.java
@@ -1,11 +1,12 @@
 package core;
 
 public interface Calculator<T> {
-    T add(final T t1, final T t2);
 
-    T subtract(final T t1, final T t2);
+  T add(final T t1, final T t2);
 
-    T multiply(final T t1, final T t2);
+  T subtract(final T t1, final T t2);
 
-    T divide(final T t1, final T t2);
+  T multiply(final T t1, final T t2);
+
+  T divide(final T t1, final T t2);
 }
