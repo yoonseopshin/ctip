428d4e120e031b3d46edab33c5cfaa26a72cc652
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-03-28 19:55:50 +0900

    Update workflow & add gui sample

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,9 +1,18 @@
 package core;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    JFrame frame = new JFrame("CTIP");
+    frame.setSize(300, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    JLabel label = new JLabel("Hello world!", SwingConstants.CENTER);
+    frame.getContentPane().add(label);
   }
 
 }
