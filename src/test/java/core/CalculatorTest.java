70ba4d51311f2abdace78112615678aaac03040f
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-04-29 13:08:27 +0900

    Test: Unit test failure

--- src/test/java/core/CalculatorTest.java
+++ src/test/java/core/CalculatorTest.java
@@ -37,7 +37,8 @@ public class CalculatorTest {
     Assert.assertEquals(3, result);
   }
 
-  @Test(expected = ArithmeticException.class)
+//  @Test(expected = ArithmeticException.class)
+  @Test()
   public void testWrongDivision() {
     calculator.divide(201511271, 0);
     Assert.fail("It doesn't occur.");
