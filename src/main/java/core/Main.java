4ac1040b343bdbb173d534600c433aced1987649
seungmin001 <51369785+seungmin001@users.noreply.github.com>
2021-04-30 19:03:55 +0900

    delete dirty code

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -16,16 +16,6 @@ public class Main {
     frame.setVisible(true);
     JLabel label = new JLabel("Hello world!", SwingConstants.CENTER);
     frame.getContentPane().add(label);
-
-    // Dirty code test.
-    Object arr[] = {"3", 16L};;;;;;;
-    System.out.println(arr[0].toString() + arr[1]);;
-
-    // Dirty code test 2: integer overflow.
-    // 0x7fffffff = Integer.MAX_VALUE
-    int a = 0x7fffffff;
-    int b = 1000;
-    assert a + b > Integer.MAX_VALUE;
   }
 
 }
