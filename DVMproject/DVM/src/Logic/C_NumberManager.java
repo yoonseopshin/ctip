a1ff5cb2a9b7a52452143981c127444434b4c9e6
Haen1231 <63691786+Haen1231@users.noreply.github.com>
2021-05-11 21:00:35 +0900

    ...

--- DVMproject/DVM/src/Logic/C_NumberManager.java
+++ DVMproject/DVM/src/Logic/C_NumberManager.java
@@ -9,29 +9,59 @@ public class C_NumberManager extends C_Number {
 	}
 	public C_NumberManager() {
 	}
-	
-	
-	//������ȣ�� C_number(title�� dvmID�� ������ ����)�� �ؽ������� ����
+
+
+	//인증번호와 C_number(title과 dvmID를 가지고 있음)를 해쉬맵으로 연결
 	HashMap<Integer, C_Number> C_List = new HashMap<Integer, C_Number>();
-    
-    public void CreateCnumber(Title ti, int DvmID) {
-        // TODO implement here
-    	C_Number_t =(int)(Math.random()*100000 + 1);  //������ȣ ����
-    	C_Number cn = new C_Number(ti, DvmID);
-    	C_List.put(C_Number_t, cn);
-    	//sendMessage()
-    	
-    }
 
-    public void PopCnumber(int C_Number_t) {
-        C_List.remove(C_Number_t); // key�� C_Number_t�� �� ����
-    }
+	public void CreateCnumber(Title ti, int DvmID) {
+		// TODO implement here
+		String numStr = "";
+		do {
+			numStr = randnumber();
+		}while(numStr.equals("000000") || numStr.equals("111111"));
+		C_Number_t = Integer.parseInt(numStr);
+		C_Number cn = new C_Number(ti, DvmID);
+		C_List.put(C_Number_t, cn);
+		//sendMessage()
 
-   
-    public boolean CheckCnumber(int C_Number_t) {
-       return C_List.containsKey(C_Number_t); //�� ����� true��ȯ
-    }
-    
-    
+	}
+
+	public String randnumber() {
+
+		Random rand = new Random();
+		int len = 6;
+		String numStr = ""; //난수가 저장될 변수
+		for(int i=0;i<len;i++) {
+			//0~9 까지 난수 생성
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+
+		return numStr;
+	}
+
+
+	public void PopCnumber(int C_Number_t) {
+		C_List.remove(C_Number_t); // key값 C_Number_t인 값 제거
+	}
+
+
+	public int CheckCnumber(int C_Number_t) {
+		if(M_Number == C_Number_t) {
+			return 0;
+		}else if(C_List.containsKey(C_Number_t)) { //존재하는 인증번호 true반환
+			return 1;
+		}else {
+			return 2;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "C_Number [음료명 :"+ti.Name + ", 자판기 id=" + DvmID + ", 인증번호=" + C_Number_t
+				+ "]";
+	}
 
 }
