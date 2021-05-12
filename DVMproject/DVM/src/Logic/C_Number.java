47edc16cff1471df0d2961e58b24341dfcabe627
Haen1231 <63691786+Haen1231@users.noreply.github.com>
2021-05-13 01:22:44 +0900

    .

--- DVMproject/DVM/src/Logic/C_Number.java
+++ DVMproject/DVM/src/Logic/C_Number.java
@@ -21,6 +21,32 @@ public class C_Number {
     	this.DvmID =id;
     }
 
+
+	public void CreateCnumber(int title_id, int DvmID) {
+		// TODO implement here
+		String numStr = "";
+		do {
+			numStr = randnumber();
+		}while(numStr.equals("000000") || numStr.equals("111111"));
+		C_Number_t = Integer.parseInt(numStr);
+
+	}
+	public String randnumber() {
+
+		Random rand = new Random();
+		int len = 6;
+		String numStr = ""; //난수가 저장될 변수
+		for(int i=0;i<len;i++) {
+			//0~9 까지 난수 생성
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+
+		return numStr;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "C_Number [����� :"+title_id+1 + ", ���Ǳ� id=" + DvmID + ", ������ȣ=" + C_Number_t
--- DVMproject/DVM/src/Logic/C_NumberManager.java
+++ DVMproject/DVM/src/Logic/C_NumberManager.java
@@ -16,33 +16,6 @@ public class C_NumberManager extends C_Number {
 	//인증번호와 C_number(title과 dvmID를 가지고 있음)를 해쉬맵으로 연결.
 	HashMap<Integer, C_Number> C_List = new HashMap<Integer, C_Number>();
 
-	public void CreateCnumber(int title_id, int DvmID) {
-		// TODO implement here
-		String numStr = "";
-		do {
-			numStr = randnumber();
-		}while(numStr.equals("000000") || numStr.equals("111111"));
-		super.C_Number_t = Integer.parseInt(numStr);
-		C_Number cn = new C_Number(title_id, DvmID);
-		C_List.put(super.C_Number_t, cn);
-		//sendMessage()
-
-	}
-
-	public String randnumber() {
-
-		Random rand = new Random();
-		int len = 6;
-		String numStr = ""; //난수가 저장될 변수
-		for(int i=0;i<len;i++) {
-			//0~9 까지 난수 생성
-			String ran = Integer.toString(rand.nextInt(10));
-			numStr += ran;
-		}
-
-		return numStr;
-	}
-
 
 	public void PopCnumber(int C_Number_t) {
 		C_List.remove(C_Number_t); // key값 C_Number_t인 값 제거
