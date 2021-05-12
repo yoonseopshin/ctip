771ab7f78631cf957a86c5c292c29a287a487208
ghdud119 <ghdud119@gmail.com>
2021-05-13 01:41:57 +0900

    ..

--- DVMproject/DVM/src/Logic/Message.java
+++ DVMproject/DVM/src/Logic/Message.java
@@ -2,7 +2,9 @@ package Logic;
 
 public class Message
 {
-    int ID = -1;
+    // 타입 1=재고확인요청(타이틀) 2 = 재고응답(타이틀, 불리안) 3 = 인증번호(타이틀, 인증번호)
+    int myID = -1;
+    int targetID = -1;
     int Type = -1;
     int xAdress = -1;
     int yAdress = -1;
@@ -11,30 +13,35 @@ public class Message
 
     boolean boolData = false;
 
+    Message(int myid)
+    {
+        myID = myid;
+    }
+
     void setmsg(int id, int type, int data)
     {
-        ID = id;
+        myID = id;
         Type = type;
         if(Type == 1 ||Type == 6)
             Title = data;
     }
     void setmsg(int id, int type, boolean data)
     {
-        ID = id;
+        targetID = id;
         Type = type;
         if(Type == 2 ||Type == 7)
             boolData = data;
     }
     void setmsg(int id, int type, int intdata, boolean booldata)
     {
-        ID = id;
+        targetID = id;
         Type = type;
         Title = intdata;
         boolData = booldata;
     }
     void setmsg(int id, int type, int data1, int data2)
     {
-        ID = id;
+        targetID = id;
         Type = type;
         if(Type == 3)
         {
