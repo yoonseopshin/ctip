d97c72bed06389e0b0a74a762549323311a4fef7
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-05-13 16:47:28 +0900

    Add project

---
+++ src/main/java/GUI/InfoCnumberErrUI.java
@@ -0,0 +1,73 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class InfoCnumberErrUI extends JFrame implements ActionListener{
+	private int s=5;
+	private Timer timer = new Timer(1000, new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			label.setText(s+"초 후 이전화면으로 돌아갑니다.");
+			if(s==0) {
+				return_value=0;
+				timer.stop();
+			}
+			s--;
+		}
+	});
+	private JButton confirm;
+	private JLabel label;
+	public int return_value= -1;
+	
+	public InfoCnumberErrUI() {
+		timer.start();
+		
+		this.setPreferredSize(new Dimension(600,800));
+		this.setTitle("DVM");
+		
+		//라벨패널
+		JPanel labelpanel = new JPanel();
+		labelpanel.setPreferredSize(new Dimension(600,300));
+		label = new JLabel();
+		label.setFont(label.getFont().deriveFont(15.0f));
+		labelpanel.add(label);
+		//안내패널
+		JPanel informpanel = new JPanel();
+		//informpanel.setPreferredSize(new Dimension(600,300));
+		JLabel infolabel = new JLabel("<html><center>해당 인증번호에 대한 선결제 정보를 확인할 수 없습니다."
+				+ "<br>다시 입력하세요</center></html>");
+		infolabel.setFont(infolabel.getFont().deriveFont(20.0f));
+		informpanel.add(infolabel);
+		//버튼패널
+		JPanel buttonpanel = new JPanel();
+		confirm=new JButton("확인");
+		confirm.setPreferredSize(new Dimension(300,70));
+		confirm.addActionListener(this);
+		buttonpanel.add(confirm);
+		
+		add(labelpanel,BorderLayout.NORTH);
+		add(informpanel,BorderLayout.CENTER);
+		add(buttonpanel,BorderLayout.SOUTH);
+		
+		pack();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);		
+	}
+	/*test
+	public static void main(String[] args) {
+		new InfoCnumberErrUI();
+
+	}
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==confirm) {
+			return_value=0;
+			timer.stop();
+		}
+	}
+}
---
+++ src/main/java/Logic/C_Number.java
@@ -0,0 +1,90 @@
+package Logic;
+
+import java.util.Random;
+
+/**
+ * 
+ */
+public class C_Number {
+
+	int title_id;
+    int DvmID;
+    int M_Number=111111;
+    int C_Number_t;
+    
+    public C_Number() {
+		
+	}
+
+	public C_Number(int title_id, int id) {
+    	this.title_id = title_id;
+    	this.DvmID =id;
+    }
+
+
+	public void CreateCnumber(int title_id, int DvmID) {
+		// TODO implement here
+		String numStr = "";
+		do {
+			numStr = randnumber();
+		}while(numStr.equals("000000") || numStr.equals("111111"));
+		C_Number_t = Integer.parseInt(numStr);
+
+	}
+	public String randnumber() {
+
+		Random rand = new Random();
+		int len = 6;
+		String numStr = ""; //난수가 저장될 변수
+		for(int i=0;i<len;i++) {
+			//0~9 까지 난수 생성
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+
+		return numStr;
+	}
+
+
+	@Override
+	public String toString() {
+		return "C_Number [����� :"+title_id+1 + ", ���Ǳ� id=" + DvmID + ", ������ȣ=" + C_Number_t
+				+ "]";
+	}
+
+	public int getTitle_id() {
+		return title_id;
+	}
+
+	public void setTitle_id(int title_id) {
+		this.title_id = title_id;
+	}
+
+	public int getDvmID() {
+		return DvmID;
+	}
+
+	public void setDvmID(int dvmID) {
+		DvmID = dvmID;
+	}
+
+	public int getM_Number() {
+		return M_Number;
+	}
+
+	public void setM_Number(int m_Number) {
+		M_Number = m_Number;
+	}
+
+	public int getC_Number_t() {
+		return C_Number_t;
+	}
+
+	public void setC_Number_t(int c_Number_t) {
+		C_Number_t = c_Number_t;
+	}
+
+
+    
+
+}
