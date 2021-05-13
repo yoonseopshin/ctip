ecf2e059550d71dc72ce1f958ad0ce73cebf5fd7
LeeHaiLim <hailim1997@gmail.com>
2021-05-14 03:41:39 +0900

    .

--- src/test/java/Logic/C_NumberTest.java
+++ src/test/java/Logic/C_NumberTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class C_NumberTest {
-
+    private C_Number CN;
     @Test
     public void createCnumber() {
+        CN= new C_Number(1,1);
+        assertEquals(CN.CreateCnumber(1,1),CN.getC_Number_t());
     }
 }
