58ee0793d95a3b4209cbaf6668505be3e387aad8
LeeHaiLim <hailim1997@gmail.com>
2021-05-14 04:43:35 +0900

    .

---
+++ src/test/java/Logic/DVMTest.java
@@ -0,0 +1,18 @@
+package Logic;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class DVMTest{
+    private DVM dvm=new DVM(1,0.5,0.7);
+
+    @Test
+    public void testGetX() {
+        Assert.assertEquals(Double.toString(0.5),Double.toString(dvm.getX()));
+    }
+
+    public void testGetY() {
+        Assert.assertEquals(Double.toString(0.7),Double.toString(dvm.getX()));
+    }
+}
