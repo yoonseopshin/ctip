33002e91ae27b7ec399d61e398d53de939c32dc5
ghdud119 <ghdud119@gmail.com>
2021-05-14 05:10:16 +0900

    .

--- src/test/java/Logic/Message_QueueTest.java
+++ src/test/java/Logic/Message_QueueTest.java
@@ -1,6 +1,7 @@
 package Logic;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -16,11 +17,18 @@ public class Message_QueueTest {
         message = new Message(1);
         message_queue = new Message_Queue();
         message.setmsg(1, 1, 1);
-
-
+        Message_Queue.sendMsg(message);
+        Assert.assertEquals(message, message_queue.Connection.poll());
     }
 
     @Test
     public void dequeue() {
+        message = new Message(1);
+        message_queue = new Message_Queue();
+        message.setmsg(1, 1, 1);
+        Message_Queue.sendMsg(message);
+        Message_Queue.recivMsg();
+        message = (Message) Message_Queue.dequeue(1).poll();
+        Assert.assertEquals(1, message.Type);
     }
 }
