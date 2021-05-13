7b020049be69228741164e77f6d2f8b7c2ff7380
LeeHaiLim <hailim1997@gmail.com>
2021-05-14 04:15:21 +0900

    .

--- src/test/java/Logic/C_NumberTest.java
+++ src/test/java/Logic/C_NumberTest.java
@@ -1,14 +1,28 @@
 package Logic;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
 public class C_NumberTest {
-    private C_Number CN;
+    private C_Number CN=new C_Number(1,1);
+    int i=CN.CreateCnumber(1,1);
+
+
+    @Before
+    public void beforeTest() throws Exception {
+        System.out.println("Before");
+    }
     @Test
-    public void createCnumber() {
-        CN= new C_Number(1,1);
-        assertEquals(CN.CreateCnumber(1,1),CN.getC_Number_t());
+    public void getC_number_t() {
+        Assert.assertEquals(i,CN.getC_Number_t());
+    }
+    @Test
+    public void setC_number_t() {
+        CN.setC_Number_t(i);
+    }
+    @After
+    public void printAfter() throws Exception{
+        System.out.println("After");
     }
 }
