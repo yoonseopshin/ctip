9b9758f42b66ffb8cf204414adaf3692bdc0d1c8
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-05-16 23:36:26 +0900

    Style: Reformat all code

--- src/main/java/GUI/AddItemMenu.java
+++ src/main/java/GUI/AddItemMenu.java
@@ -10,127 +10,127 @@ import javax.swing.*;
 
 public class AddItemMenu extends JFrame implements ActionListener {
 
-    public Timer timer = new Timer(180000, new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            return_value = -2;
-            timer.stop();
-        }
-    });
-
-    private JComboBox<String> Yearselect;
-    private JComboBox<String> Monthselect;
-    private JComboBox<String> Dayselect;
-
-    private JButton cancel;
-    private JButton add;
-
-    public int return_value = -1;
-    public int return_date;
-
-    public AddItemMenu() {
-        timer.start();
-
-        this.setPreferredSize(new Dimension(600, 800));
-        this.setTitle("DVM");
-
-        //라벨패널
-        JPanel labelpanel = new JPanel();
-        labelpanel.setPreferredSize(new Dimension(600, 300));
-        JLabel label = new JLabel("추가할 재고의 유통기한을 입력하세요");
-        label.setFont(label.getFont().deriveFont(15.0f));
-        labelpanel.add(label);
-
-        //유통기한 입력패널
-        JPanel expdatepanel = new JPanel();
-        JLabel y = new JLabel("년   ");
-        JLabel m = new JLabel("월   ");
-        JLabel d = new JLabel("일");
-        y.setFont(y.getFont().deriveFont(20.0f));
-        m.setFont(m.getFont().deriveFont(20.0f));
-        d.setFont(d.getFont().deriveFont(20.0f));
-
-        //선택창
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        String[] year_list = new String[50];
-        String[] month_list = new String[12];
-        String[] day_list = new String[31];
-        for (int i = 0; i < year_list.length; i++) {
-            year_list[i] = Integer.toString(year + i);
-        }
-        for (int i = 0; i < month_list.length; i++) {
-            if (i + 1 < 10) {
-                month_list[i] = "0".concat(Integer.toString(i + 1));
-            } else {
-                month_list[i] = Integer.toString(i + 1);
-            }
-        }
-        for (int i = 0; i < day_list.length; i++) {
-            if (i + 1 < 10) {
-                day_list[i] = "0".concat(Integer.toString(i + 1));
-            } else {
-                day_list[i] = Integer.toString(i + 1);
-            }
-        }
-        Yearselect = new JComboBox<String>(year_list);
-        Monthselect = new JComboBox<String>(month_list);
-        Dayselect = new JComboBox<String>(day_list);
-        Yearselect.setFont(Yearselect.getFont().deriveFont(20.0f));
-        Monthselect.setFont(Monthselect.getFont().deriveFont(20.0f));
-        Dayselect.setFont(Dayselect.getFont().deriveFont(20.0f));
-        Yearselect.addActionListener(this);
-        Monthselect.addActionListener(this);
-        Dayselect.addActionListener(this);
-        expdatepanel.add(Yearselect);
-        expdatepanel.add(y);
-        expdatepanel.add(Monthselect);
-        expdatepanel.add(m);
-        expdatepanel.add(Dayselect);
-        expdatepanel.add(d);
-        //버튼패널
-        JPanel buttonpanel = new JPanel(new GridLayout(1, 2));
-        buttonpanel.setPreferredSize(new Dimension(600, 70));
-        cancel = new JButton("취소");
-        add = new JButton("추가");
-        cancel.addActionListener(this);
-        add.addActionListener(this);
-        buttonpanel.add(add);
-        buttonpanel.add(cancel);
-
-        add(labelpanel, BorderLayout.NORTH);
-        add(expdatepanel, BorderLayout.CENTER);
-        add(buttonpanel, BorderLayout.SOUTH);
-
-        pack();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-
-
-    }
-
-    /*test
-    public static void main(String[] args) {
-      new AddItemMenu();
-    }
-  */
+  public Timer timer = new Timer(180000, new ActionListener() {
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == Yearselect || e.getSource() == Monthselect || e.getSource() == Dayselect) {
-            timer.restart();
-        }
-        if (e.getSource() == add) {
-            String y = (String) Yearselect.getSelectedItem();
-            String m = (String) Monthselect.getSelectedItem();
-            String d = (String) Dayselect.getSelectedItem();
-            return_date = Integer.parseInt(y.concat(m).concat(d));
-            timer.stop();
-            return_value = 1;
-        }
-        if (e.getSource() == cancel) {
-            timer.stop();
-            return_value = 0;
-        }
+      return_value = -2;
+      timer.stop();
     }
+  });
+
+  private JComboBox<String> Yearselect;
+  private JComboBox<String> Monthselect;
+  private JComboBox<String> Dayselect;
+
+  private JButton cancel;
+  private JButton add;
+
+  public int return_value = -1;
+  public int return_date;
+
+  public AddItemMenu() {
+    timer.start();
+
+    this.setPreferredSize(new Dimension(600, 800));
+    this.setTitle("DVM");
+
+    //라벨패널
+    JPanel labelpanel = new JPanel();
+    labelpanel.setPreferredSize(new Dimension(600, 300));
+    JLabel label = new JLabel("추가할 재고의 유통기한을 입력하세요");
+    label.setFont(label.getFont().deriveFont(15.0f));
+    labelpanel.add(label);
+
+    //유통기한 입력패널
+    JPanel expdatepanel = new JPanel();
+    JLabel y = new JLabel("년   ");
+    JLabel m = new JLabel("월   ");
+    JLabel d = new JLabel("일");
+    y.setFont(y.getFont().deriveFont(20.0f));
+    m.setFont(m.getFont().deriveFont(20.0f));
+    d.setFont(d.getFont().deriveFont(20.0f));
+
+    //선택창
+    Calendar cal = Calendar.getInstance();
+    int year = cal.get(Calendar.YEAR);
+    String[] year_list = new String[50];
+    String[] month_list = new String[12];
+    String[] day_list = new String[31];
+    for (int i = 0; i < year_list.length; i++) {
+      year_list[i] = Integer.toString(year + i);
+    }
+    for (int i = 0; i < month_list.length; i++) {
+      if (i + 1 < 10) {
+        month_list[i] = "0".concat(Integer.toString(i + 1));
+      } else {
+        month_list[i] = Integer.toString(i + 1);
+      }
+    }
+    for (int i = 0; i < day_list.length; i++) {
+      if (i + 1 < 10) {
+        day_list[i] = "0".concat(Integer.toString(i + 1));
+      } else {
+        day_list[i] = Integer.toString(i + 1);
+      }
+    }
+    Yearselect = new JComboBox<String>(year_list);
+    Monthselect = new JComboBox<String>(month_list);
+    Dayselect = new JComboBox<String>(day_list);
+    Yearselect.setFont(Yearselect.getFont().deriveFont(20.0f));
+    Monthselect.setFont(Monthselect.getFont().deriveFont(20.0f));
+    Dayselect.setFont(Dayselect.getFont().deriveFont(20.0f));
+    Yearselect.addActionListener(this);
+    Monthselect.addActionListener(this);
+    Dayselect.addActionListener(this);
+    expdatepanel.add(Yearselect);
+    expdatepanel.add(y);
+    expdatepanel.add(Monthselect);
+    expdatepanel.add(m);
+    expdatepanel.add(Dayselect);
+    expdatepanel.add(d);
+    //버튼패널
+    JPanel buttonpanel = new JPanel(new GridLayout(1, 2));
+    buttonpanel.setPreferredSize(new Dimension(600, 70));
+    cancel = new JButton("취소");
+    add = new JButton("추가");
+    cancel.addActionListener(this);
+    add.addActionListener(this);
+    buttonpanel.add(add);
+    buttonpanel.add(cancel);
+
+    add(labelpanel, BorderLayout.NORTH);
+    add(expdatepanel, BorderLayout.CENTER);
+    add(buttonpanel, BorderLayout.SOUTH);
+
+    pack();
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+
+
+  }
+
+  /*test
+  public static void main(String[] args) {
+    new AddItemMenu();
+  }
+*/
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == Yearselect || e.getSource() == Monthselect || e.getSource() == Dayselect) {
+      timer.restart();
+    }
+    if (e.getSource() == add) {
+      String y = (String) Yearselect.getSelectedItem();
+      String m = (String) Monthselect.getSelectedItem();
+      String d = (String) Dayselect.getSelectedItem();
+      return_date = Integer.parseInt(y.concat(m).concat(d));
+      timer.stop();
+      return_value = 1;
+    }
+    if (e.getSource() == cancel) {
+      timer.stop();
+      return_value = 0;
+    }
+  }
 
 }
--- src/main/java/GUI/FindingDVM.java
+++ src/main/java/GUI/FindingDVM.java
@@ -6,76 +6,77 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class FindingDVM extends JFrame implements ActionListener {
-    private int s = 32;
-    private Timer timer = new Timer(1000, new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            if (s <= 3) {
-                label.setText("남은 시간: 0");
-                infolabel.setText("<html><center>자판기를 찾는 것에 실패하였습니다<br>"
-                        + s + "초 후 메인화면으로 돌아갑니다.</center></html>");
-                if (s == 0) {
-                    return_value = 0;
-                    timer.stop();
-                }
-            } else {
-                label.setText("남은 시간: " + (s - 3));
-            }
-            s--;
-        }
-    });
 
-    private JLabel label;
-    public int return_value = -1;
-    private JLabel infolabel;
-    private JButton cancel;
-
-    public FindingDVM(String name) {
-        timer.start();
-
-        this.setPreferredSize(new Dimension(600, 800));
-        this.setTitle("DVM");
-
-        //라벨패널
-        JPanel labelpanel = new JPanel();
-        labelpanel.setPreferredSize(new Dimension(600, 300));
-        label = new JLabel("남은시간:20");
-        label.setFont(label.getFont().deriveFont(15.0f));
-        labelpanel.add(label);
-        //안내패널
-        JPanel informpanel = new JPanel();
-        //informpanel.setPreferredSize(new Dimension(600,300));
-        infolabel = new JLabel("<html><center><strong>" + name + "</strong>"
-                + "<br>해당 음료가 있는 자판기를 검색중입니다.</center></html>");
-        infolabel.setFont(infolabel.getFont().deriveFont(20.0f));
-        informpanel.add(infolabel);
-        //버튼패널
-        JPanel buttonpanel = new JPanel();
-        cancel = new JButton("취소");
-        cancel.setPreferredSize(new Dimension(300, 70));
-        cancel.addActionListener(this);
-        buttonpanel.add(cancel);
-
-        add(labelpanel, BorderLayout.NORTH);
-        add(informpanel, BorderLayout.CENTER);
-        add(buttonpanel, BorderLayout.SOUTH);
-
-        pack();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-
-    /*
-    public static void main(String[] args) {
-      new FindingDVM("코카콜라");
-
-    }
-    */
-    @Override
+  private int s = 32;
+  private Timer timer = new Timer(1000, new ActionListener() {
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == cancel) {
-            timer.stop();
-            return_value = 0;
+      if (s <= 3) {
+        label.setText("남은 시간: 0");
+        infolabel.setText("<html><center>자판기를 찾는 것에 실패하였습니다<br>"
+            + s + "초 후 메인화면으로 돌아갑니다.</center></html>");
+        if (s == 0) {
+          return_value = 0;
+          timer.stop();
         }
+      } else {
+        label.setText("남은 시간: " + (s - 3));
+      }
+      s--;
     }
+  });
+
+  private JLabel label;
+  public int return_value = -1;
+  private JLabel infolabel;
+  private JButton cancel;
+
+  public FindingDVM(String name) {
+    timer.start();
+
+    this.setPreferredSize(new Dimension(600, 800));
+    this.setTitle("DVM");
+
+    //라벨패널
+    JPanel labelpanel = new JPanel();
+    labelpanel.setPreferredSize(new Dimension(600, 300));
+    label = new JLabel("남은시간:20");
+    label.setFont(label.getFont().deriveFont(15.0f));
+    labelpanel.add(label);
+    //안내패널
+    JPanel informpanel = new JPanel();
+    //informpanel.setPreferredSize(new Dimension(600,300));
+    infolabel = new JLabel("<html><center><strong>" + name + "</strong>"
+        + "<br>해당 음료가 있는 자판기를 검색중입니다.</center></html>");
+    infolabel.setFont(infolabel.getFont().deriveFont(20.0f));
+    informpanel.add(infolabel);
+    //버튼패널
+    JPanel buttonpanel = new JPanel();
+    cancel = new JButton("취소");
+    cancel.setPreferredSize(new Dimension(300, 70));
+    cancel.addActionListener(this);
+    buttonpanel.add(cancel);
+
+    add(labelpanel, BorderLayout.NORTH);
+    add(informpanel, BorderLayout.CENTER);
+    add(buttonpanel, BorderLayout.SOUTH);
+
+    pack();
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
+  /*
+  public static void main(String[] args) {
+    new FindingDVM("코카콜라");
+
+  }
+  */
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == cancel) {
+      timer.stop();
+      return_value = 0;
+    }
+  }
 
 }
