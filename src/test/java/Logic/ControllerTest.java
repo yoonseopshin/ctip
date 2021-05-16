7645c7ebce91b1036b7956a0ddd7ea8f8b28f4d0
LeeHaiLim <hailim1997@gmail.com>
2021-05-16 14:25:57 +0900

    .

---
+++ src/test/java/Logic/ControllerTest.java
@@ -0,0 +1,21 @@
+package Logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+    private Controller c=new Controller();
+
+    @Test
+    public void testInit() {
+        c.setBasket(10);
+        c.setPayment(new Payment(c.getBasket(),1));
+        c.init();
+        int ExpectedResult=-666;
+        int ActualResult=c.getBasket();
+        Assert.assertEquals(ExpectedResult,ActualResult);
+        Assert.assertNull(c.getPayment());
+    }
+}
---
+++ src/test/java/Logic/MessageTest.java
@@ -0,0 +1,52 @@
+package Logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageTest {
+    Message msg = new Message(1);
+
+    @Test
+    public void testsetmsg1() {
+        msg.setmsg(1,1,1);
+        Assert.assertEquals(1,msg.getTargetID());
+        Assert.assertEquals(1,msg.getType());
+        Assert.assertEquals(1,msg.getTitle());
+    }
+
+    @Test
+    public void testSetmsg2() {
+        msg.setmsg(1,2,true);
+        Assert.assertEquals(1,msg.getTargetID());
+        Assert.assertEquals(2,msg.getType());
+        Assert.assertTrue(msg.isBoolData());
+    }
+
+    @Test
+    public void testSetmsg3() {
+        msg.setmsg(1,3);
+        Assert.assertEquals(1,msg.getTargetID());
+        Assert.assertEquals(3,msg.getType());
+    }
+
+    @Test
+    public void testSetmsg4() {
+        msg.setmsg(1,4,1.0,1.0);
+        Assert.assertEquals(1,msg.getTargetID());
+        Assert.assertEquals(4,msg.getType());
+        Assert.assertEquals(Double.toString(1.0),Double.toString(msg.getxAdress()));
+        Assert.assertEquals(Double.toString(1.0),Double.toString(msg.getyAdress()));
+    }
+
+    @Test
+    public void testSetmsg5() {
+        msg.setmsg(1,5,1,971125);
+        Assert.assertEquals(1,msg.getTargetID());
+        Assert.assertEquals(5,msg.getType());
+        Assert.assertEquals(1,msg.getTitle());
+        Assert.assertEquals(971125,msg.getC_Number());
+
+    }
+}
