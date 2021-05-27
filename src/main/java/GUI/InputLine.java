f4031a7784216454da894b1fb0ed26f3e1f4e73b
LeeHaiLim <hailim1997@gmail.com>
2021-05-27 10:13:38 +0900

    sonarcloud bug fix

--- src/main/java/GUI/InputLine.java
+++ src/main/java/GUI/InputLine.java
@@ -14,7 +14,6 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import javax.swing.text.JTextComponent;
 
 public class InputLine extends JFrame implements ActionListener {
 
--- src/main/java/Logic/C_Number.java
+++ src/main/java/Logic/C_Number.java
@@ -9,11 +9,12 @@ public class C_Number {
     private int title_id;
     private int DvmID;
     private int C_Number_t;
-    private Random rand;
+    private Random rand;;
 
     {
         try {
-            rand = new SecureRandom().getInstanceStrong();
+            new SecureRandom();
+            rand = SecureRandom.getInstanceStrong();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Failed to instantiate random number generator", e);
         }
@@ -54,8 +55,6 @@ public class C_Number {
     }
 
     public String randnumber() {
-
-        Random rand = new Random();
         int len = 6;
         String numStr = ""; //난수가 저장될 변수
         String ran = Integer.toString(rand.nextInt(9) + 1);  //첫번째 숫자 0이 아님
